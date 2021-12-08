3f82fb27842301e6c5e3220fd9968adcfda607cc
Kenji Loo <kenjilooxy@gmail.com>
2021-12-08 15:22:14 +0800

    Renamed the exit button variables

--- src/main/java/main/gameRunner/homeMenu/HomeMenu.java
+++ src/main/java/main/gameRunner/homeMenu/HomeMenu.java
@@ -40,7 +40,8 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
     private static final String GAME_TITLE = "Brick Destroy";
     private static final String CREDITS = "Version 0.1.1";
     private static final String START_TEXT = "Start";
-    private static final String MENU_TEXT = "Exit";
+    private static final String EXIT_TEXT = "Exit";
+    private static final String INSTRUCTION_TEXT = "Control";
     private static final Color BG_COLOR = Color.GREEN.darker();
     private static final Color BORDER_COLOR = new Color(200,8,21); //Venetian Red
     private static final Color DASH_BORDER_COLOR = new  Color(255, 216, 0);//school bus yellow
@@ -51,7 +52,8 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
     private static final float[] DASHES = {12,6};
     private Rectangle menuFace;
     private Rectangle startButton;
-    private Rectangle menuButton;
+    private Rectangle exitButton;
+    private Rectangle instructionButton;
     private BasicStroke borderStoke;
     private BasicStroke borderStoke_noDashes;
     private Font greetingsFont;
@@ -61,6 +63,7 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
     private GameFrame owner;
     private boolean startClicked;
     private boolean menuClicked;
+    private boolean instructionClicked;
     private boolean drawImage = true;
 
     /**
@@ -83,7 +86,8 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
 
         Dimension btnDim = new Dimension(area.width / 3, area.height / 12);
         startButton = new Rectangle(btnDim);
-        menuButton = new Rectangle(btnDim);
+        exitButton = new Rectangle(btnDim);
+        instructionButton = new Rectangle(btnDim);
 
         borderStoke = new BasicStroke(BORDER_SIZE,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND,0,DASHES,0);
         borderStoke_noDashes = new BasicStroke(BORDER_SIZE,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND);
@@ -206,13 +210,15 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
         FontRenderContext frc = g2d.getFontRenderContext();
 
         Rectangle2D txtRect = buttonFont.getStringBounds(START_TEXT,frc);
-        Rectangle2D mTxtRect = buttonFont.getStringBounds(MENU_TEXT,frc);
+        Rectangle2D mTxtRect = buttonFont.getStringBounds(EXIT_TEXT,frc);
+        Rectangle2D iTxtRect = buttonFont.getStringBounds(INSTRUCTION_TEXT,frc);
 
         g2d.setFont(buttonFont);
 
         int x = (menuFace.width - startButton.width) / 2;
         int y =(int) ((menuFace.height - startButton.height) * 0.8);
 
+        //Start Button Placement
         startButton.setLocation(x,y);
 
         x = (int)(startButton.getWidth() - txtRect.getWidth()) / 2;
@@ -239,26 +245,50 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
 
         y *= 1.2;
 
-        menuButton.setLocation(x,y);
+        //exit button placement
+        exitButton.setLocation(x,y);
 
-        x = (int)(menuButton.getWidth() - mTxtRect.getWidth()) / 2;
-        y = (int)(menuButton.getHeight() - mTxtRect.getHeight()) / 2;
+        x = (int)(exitButton.getWidth() - mTxtRect.getWidth()) / 2;
+        y = (int)(exitButton.getHeight() - mTxtRect.getHeight()) / 2;
 
-        x += menuButton.x;
-        y += menuButton.y + (startButton.height * 0.9);
+        x += exitButton.x;
+        y += exitButton.y + (startButton.height * 0.9);
 
         if(menuClicked){
             Color tmp = g2d.getColor();
 
             g2d.setColor(CLICKED_BUTTON_COLOR);
-            g2d.draw(menuButton);
+            g2d.draw(exitButton);
             g2d.setColor(CLICKED_TEXT);
-            g2d.drawString(MENU_TEXT,x,y);
+            g2d.drawString(EXIT_TEXT,x,y);
             g2d.setColor(tmp);
         }
         else{
-            g2d.draw(menuButton);
-            g2d.drawString(MENU_TEXT,x,y);
+            g2d.draw(exitButton);
+            g2d.drawString(EXIT_TEXT,x,y);
+        }
+
+        //instruction button placement
+        instructionButton.setLocation(x,y);
+
+        x = (int)(instructionButton.getWidth() - iTxtRect.getWidth()) / 2;
+        y = (int)(instructionButton.getHeight() - iTxtRect.getHeight()) / 2;
+
+        x += instructionButton.x;
+        y += instructionButton.y + (startButton.height * 0.9);
+
+        if(instructionClicked){
+            Color tmp = g2d.getColor();
+
+            g2d.setColor(CLICKED_BUTTON_COLOR);
+            g2d.draw(instructionButton);
+            g2d.setColor(CLICKED_TEXT);
+            g2d.drawString(INSTRUCTION_TEXT,x,y);
+            g2d.setColor(tmp);
+        }
+        else{
+            g2d.draw(instructionButton);
+            g2d.drawString(INSTRUCTION_TEXT,x,y);
         }
     }
 
@@ -275,9 +305,12 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
            owner.enableGameBoard();
 
         }
-        else if(menuButton.contains(p)){
+        else if(exitButton.contains(p)){
             System.out.println("Goodbye " + System.getProperty("user.name"));
             System.exit(0);
+        }
+        else if(instructionButton.contains(p)){
+            
         }
     }
 
@@ -295,9 +328,9 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
             repaint(startButton.x,startButton.y,startButton.width+1,startButton.height+1);
 
         }
-        else if(menuButton.contains(p)){
+        else if(exitButton.contains(p)){
             menuClicked = true;
-            repaint(menuButton.x,menuButton.y,menuButton.width+1,menuButton.height+1);
+            repaint(exitButton.x, exitButton.y, exitButton.width+1, exitButton.height+1);
         }
     }
 
@@ -315,7 +348,7 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
         }
         else if(menuClicked){
             menuClicked = false;
-            repaint(menuButton.x,menuButton.y,menuButton.width+1,menuButton.height+1);
+            repaint(exitButton.x, exitButton.y, exitButton.width+1, exitButton.height+1);
         }
     }
 
@@ -351,7 +384,7 @@ public class HomeMenu extends Image implements MouseListener, MouseMotionListene
     public void mouseMoved(MouseEvent mouseEvent)
     {
         Point p = mouseEvent.getPoint();
-        if(startButton.contains(p) || menuButton.contains(p))
+        if(startButton.contains(p) || exitButton.contains(p))
             this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         else
             this.setCursor(Cursor.getDefaultCursor());
