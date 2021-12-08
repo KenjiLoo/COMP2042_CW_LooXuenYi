59c3f76ecc4bc6b3a526b89451f12e1c758b1153
Kenji Loo <kenjilooxy@gmail.com>
2021-12-08 13:30:04 +0800

    Updated Copyright information

--- src/main/java/main/gameRunner/debugConsole/DebugConsole.java
+++ src/main/java/main/gameRunner/debugConsole/DebugConsole.java
@@ -2,7 +2,7 @@ package main.gameRunner.debugConsole;
 
 /*
  *  Brick Destroy - A simple Arcade video game
- *   Copyright (C) 2017  Filippo Ranza
+ *   Copyright (C) 2021  Loo Xuen Yi
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
